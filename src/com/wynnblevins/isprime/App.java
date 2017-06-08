b4fd75e66fcb23dc407dd1f0096a89b1295294c9
Wynn Blevins <wblevins@ibcdbs.com>
2017-06-08 14:10:49 -0400

    IsPrime is now a maven project that uses spring boot.  Also the app now prompts the user to enter a number rather than taking the input as a command line arg.

--- src/com/wynnblevins/isprime/App.java
+++ src/com/wynnblevins/isprime/App.java
@@ -1,28 +1,21 @@
 package com.wynnblevins.isprime;
 
+import java.util.Scanner;
+
 import com.wynnblevins.isprime.service.NormalPrimeService;
 import com.wynnblevins.isprime.service.PrimeService;
 
 public class App {
-	private static Integer parseIntArg(String[] args) {
-		Integer numberArg = null;
-		
-		if (args.length > 0) {
-			try {
-				numberArg = Integer.parseInt(args[0]);
-			}
-			catch (NumberFormatException nfe) {
-				nfe.printStackTrace();
-				System.exit(1);
-			} 
-		} 
-		
-		return numberArg;
+	private static Integer promptForInput() {
+		Scanner reader = new Scanner(System.in);		
+		System.out.print("Please enter an integer: ");		 
+		Integer n = reader.nextInt();
+		return n;
 	}
 	
 	public static void main(String[] args) {
 		PrimeService primeService = new NormalPrimeService();
-		Integer inputInteger = parseIntArg(args);		
+		Integer inputInteger = promptForInput();
 		
 		if (primeService.isPrime(inputInteger)) {
 			System.out.println(inputInteger + " is Prime.");
--- src/com/wynnblevins/isprime/service/NormalPrimeService.java
+++ src/com/wynnblevins/isprime/service/NormalPrimeService.java
@@ -4,8 +4,8 @@ public class NormalPrimeService implements PrimeService {
 
 	@Override
 	public boolean isPrime(int number) {
-		if (number % 2 == 0) return false;
 		if (number == 2) return true;	
+		if (number % 2 == 0) return false;
 		for (int i = 3; i * i <= number; i += 2)
             if (number % i == 0) return false;
         
