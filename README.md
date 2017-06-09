0adb8f276676d46ed2346ebadfd86b4f556f7a7a
Wynn Blevins <wblevins@ibcdbs.com>
2017-06-09 09:40:12 -0400

    Added comments NormalPrimeService.java, added instructions on how to run the project to README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # IsPrime
 A java solution to the "is prime" interview question. 
+
+#### How to run
+To run, navigate to via the command line to the root of the project directory (the same directory containing the pom.xml file) and run 
+
+```sh 
+mvn clean spring-boot:run
+```
+
+Once the project starts, you will be prompted to enter a number.  The program will then print output telling you if the number entered is prime or not. 
--- src/main/java/com/wynnblevins/isprime/service/NormalPrimeService.java
+++ src/main/java/com/wynnblevins/isprime/service/NormalPrimeService.java
@@ -7,15 +7,28 @@ public class NormalPrimeService implements PrimeService {
 
 	@Override
 	public boolean isPrime(Long num) {
-	    if (num < 2) 
+	    // numbers less than two do not count as prime
+		if (num < 2) 
 	    	return false;
-        if (num == 2) 
+        
+		// two is a prime number
+		if (num == 2) 
         	return true;
-        if (num % 2 == 0) 
+        
+		// if the number is even it then it can't be prime
+		if (num % 2 == 0) 
         	return false;
-        for (int i = 3; i * i <= num; i += 2)
-            if (num % i == 0) 
+        
+		// starting at three, 
+		// while i * i is less than the number we're checking against
+		// increment the number by 2 
+		for (int i = 3; i * i <= num; i += 2) {
+            
+			// then check if the number is divisible by 1 
+			if (num % i == 0) 
             	return false;
+		}
+		
         return true;
 	}
 	
